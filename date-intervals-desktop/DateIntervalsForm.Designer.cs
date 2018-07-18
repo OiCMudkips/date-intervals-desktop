c55c9a471fc77827dd182ba131f48103d3d380e9
OiCMudkips <victor_zhou1995@hotmail.ca>
2018-07-17 23:58:44 -0400

    display list of dates; change max interval length
    make max interval length 500 even though we probably will never make
    this request

--- date-intervals-desktop/DateIntervalsForm.Designer.cs
+++ date-intervals-desktop/DateIntervalsForm.Designer.cs
@@ -29,28 +29,28 @@
         private void InitializeComponent()
         {
             this.groupBoxOptions = new System.Windows.Forms.GroupBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.numPickerIntervalLength = new System.Windows.Forms.NumericUpDown();
+            this.numPickerDates = new System.Windows.Forms.NumericUpDown();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.lblStartDate = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.numPickerDates = new System.Windows.Forms.NumericUpDown();
-            this.nunPickerIntervalLength = new System.Windows.Forms.NumericUpDown();
-            this.btnGenerate = new System.Windows.Forms.Button();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.groupBoxOptions.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPickerIntervalLength)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numPickerDates)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nunPickerIntervalLength)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBoxOptions
             // 
             this.groupBoxOptions.AutoSize = true;
             this.groupBoxOptions.Controls.Add(this.btnGenerate);
-            this.groupBoxOptions.Controls.Add(this.nunPickerIntervalLength);
+            this.groupBoxOptions.Controls.Add(this.numPickerIntervalLength);
             this.groupBoxOptions.Controls.Add(this.numPickerDates);
             this.groupBoxOptions.Controls.Add(this.label2);
             this.groupBoxOptions.Controls.Add(this.label1);
             this.groupBoxOptions.Controls.Add(this.lblStartDate);
-            this.groupBoxOptions.Controls.Add(this.dateTimePicker1);
+            this.groupBoxOptions.Controls.Add(this.dateTimePicker);
             this.groupBoxOptions.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBoxOptions.Location = new System.Drawing.Point(0, 0);
             this.groupBoxOptions.Name = "groupBoxOptions";
@@ -58,6 +58,57 @@
             this.groupBoxOptions.TabIndex = 0;
             this.groupBoxOptions.TabStop = false;
             this.groupBoxOptions.Text = "Options";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(140, 126);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 6;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // numPickerIntervalLength
+            // 
+            this.numPickerIntervalLength.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numPickerIntervalLength.Location = new System.Drawing.Point(232, 88);
+            this.numPickerIntervalLength.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numPickerIntervalLength.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numPickerIntervalLength.Name = "numPickerIntervalLength";
+            this.numPickerIntervalLength.Size = new System.Drawing.Size(120, 20);
+            this.numPickerIntervalLength.TabIndex = 5;
+            this.numPickerIntervalLength.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numPickerDates
+            // 
+            this.numPickerDates.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numPickerDates.Location = new System.Drawing.Point(232, 56);
+            this.numPickerDates.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numPickerDates.Name = "numPickerDates";
+            this.numPickerDates.Size = new System.Drawing.Size(120, 20);
+            this.numPickerDates.TabIndex = 4;
+            this.numPickerDates.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // label2
             // 
@@ -86,40 +137,14 @@
             this.lblStartDate.TabIndex = 1;
             this.lblStartDate.Text = "Start Date";
             // 
-            // dateTimePicker1
+            // dateTimePicker
             // 
-            this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(152, 19);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker1.TabIndex = 0;
-            // 
-            // numPickerDates
-            // 
-            this.numPickerDates.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.numPickerDates.Location = new System.Drawing.Point(232, 56);
-            this.numPickerDates.Name = "numPickerDates";
-            this.numPickerDates.Size = new System.Drawing.Size(120, 20);
-            this.numPickerDates.TabIndex = 4;
-            // 
-            // nunPickerIntervalLength
-            // 
-            this.nunPickerIntervalLength.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.nunPickerIntervalLength.Location = new System.Drawing.Point(232, 88);
-            this.nunPickerIntervalLength.Name = "nunPickerIntervalLength";
-            this.nunPickerIntervalLength.Size = new System.Drawing.Size(120, 20);
-            this.nunPickerIntervalLength.TabIndex = 5;
-            // 
-            // btnGenerate
-            // 
-            this.btnGenerate.Location = new System.Drawing.Point(140, 126);
-            this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
-            this.btnGenerate.TabIndex = 6;
-            this.btnGenerate.Text = "Generate";
-            this.btnGenerate.UseVisualStyleBackColor = true;
-            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            this.dateTimePicker.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker.Location = new System.Drawing.Point(152, 19);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker.TabIndex = 0;
             // 
             // DateIntervalsForm
             // 
@@ -131,8 +156,8 @@
             this.Text = "Date Intervals";
             this.groupBoxOptions.ResumeLayout(false);
             this.groupBoxOptions.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPickerIntervalLength)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numPickerDates)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nunPickerIntervalLength)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,12 +166,12 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBoxOptions;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblStartDate;
         private System.Windows.Forms.NumericUpDown numPickerDates;
-        private System.Windows.Forms.NumericUpDown nunPickerIntervalLength;
+        private System.Windows.Forms.NumericUpDown numPickerIntervalLength;
         private System.Windows.Forms.Button btnGenerate;
     }
 }
--- date-intervals-desktop/DateIntervalsForm.cs
+++ date-intervals-desktop/DateIntervalsForm.cs
@@ -19,7 +19,43 @@ namespace date_intervals_desktop
 
         private void btnGenerate_Click(object sender, EventArgs e)
         {
-            // TODO: Open Interval Window
+            var listOfDates = MakeListOfDates(dateTimePicker.Value, (int)numPickerDates.Value, (int)numPickerIntervalLength.Value);
+            DatesForm datesForm = new DatesForm(listOfDates);
+            datesForm.Show();
+        }
+
+        /// <summary>
+        /// Creates a list of dates specified by the input parameters.
+        /// </summary>
+        /// <param name="startDate">The first date of the list of dates.</param>
+        /// <param name="numDates">Number of dates to include in the final list.</param>
+        /// <param name="daysBetweenDates">Number of days between each date in the final list.</param>
+        /// <returns></returns>
+        protected IList<DateTime> MakeListOfDates(DateTime startDate, int numDates, int daysBetweenDates)
+        {
+            if (startDate == null)
+            {
+                throw new ArgumentNullException("startDate");
+            }
+
+            if (numDates < 1)
+            {
+                throw new ArgumentOutOfRangeException("numDates");
+            }
+
+            if (daysBetweenDates < 1)
+            {
+                throw new ArgumentOutOfRangeException("daysBetweenDates");
+            }
+
+            var listOfDates = new List<DateTime>(numDates);
+            for (int i = 0; i < numDates; i++)
+            {
+                int daysToAddToStartDate = i * daysBetweenDates;
+                listOfDates.Add(startDate.AddDays(daysToAddToStartDate));
+            }
+
+            return listOfDates;
         }
     }
 }
